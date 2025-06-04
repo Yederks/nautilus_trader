00c3c9decf142923c1a7a439479acbb8c349170a
Chris Sellers <chris@cjdsellers.io>
2025-06-04 13:56:24 +1000

    Fix windows build

--- build.py
+++ build.py
@@ -94,6 +94,10 @@ if IS_LINUX and IS_ARM64:
 if IS_WINDOWS:
     # Linker error 1181
     # https://docs.microsoft.com/en-US/cpp/error-messages/tool-errors/linker-tools-error-lnk1181?view=msvc-170&viewFallbackFrom=vs-2019
+    os.environ["CC"] = "clang-cl"
+    os.environ["CXX"] = "clang-cl"
+    os.environ["LD"] = "lld-link"
+
     RUST_LIB_PFX = ""
     RUST_STATIC_LIB_EXT = "lib"
     RUST_DYLIB_EXT = "dll"
